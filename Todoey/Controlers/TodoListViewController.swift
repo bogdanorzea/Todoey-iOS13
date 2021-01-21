e75f95c182238cd14f0b0c5e057a45858a72a488
Bogdan Orzea <bogdan.orzea@gmail.com>
2021-01-21 17:51:50 -0500

    Adds item storage using UserDefaults

---
+++ Todoey/Controlers/TodoListViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var itemArray = [Item]()
+    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
+    }
+
+    // MARK: - TabelView DataSource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+
+        // Configure the cell’s content
+        cell.textLabel!.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+
+        return cell
+    }
+
+    // MARK: - TableView Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+        tableView.reloadData()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // MARK: - Add new item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Todoy item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add item", style: .default) { action in
+
+            let newItem = Item()
+            newItem.title = textField.text!
+
+            self.itemArray.append(newItem)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.tableView.reloadData()
+        }
+
+        alert.addAction(action)
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+
+        present(alert, animated: true)
+    }
+}
+
